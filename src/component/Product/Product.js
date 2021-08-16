f1a4d655a0fb66607f840046e08914c330c9a78c
Robin <mdrobinhossain12@gmail.com>
2021-08-17 01:41:33 +0600

    Before Making Cart component

---
+++ src/component/Product/Product.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+const Product = (props) => {
+    const {img, name, seller, price, stock} =props.product;
+    return (
+        <div className="product">
+            
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+            <h4 className="product-name">{name}</h4>
+            <br/>
+            <p><small>by: {seller}</small></p>
+            <p>Price: ${price}</p>
+            <br />
+            <p><small>Only {stock} left in stock. Order soon</small></p>
+            <button className="main-button" onClick={() => props.handleAddProduct(props.product)}> <FontAwesomeIcon icon={faShoppingCart} />Add to Cart</button>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Product;
